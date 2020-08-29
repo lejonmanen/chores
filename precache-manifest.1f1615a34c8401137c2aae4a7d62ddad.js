1c28c4e85923fefdfba8f1818eac8ab18e3852a1
David Andersson <lejonmanen@gmail.com>
2020-08-29 12:39:28 +0200

    Updates

---
+++ precache-manifest.1f1615a34c8401137c2aae4a7d62ddad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be5faddebbb2e823ded8455bc733cf4f",
+    "url": "/chores/index.html"
+  },
+  {
+    "revision": "8924030eabcd55a5df05",
+    "url": "/chores/static/css/main.a9823bef.chunk.css"
+  },
+  {
+    "revision": "027be980dc91eb13f8ab",
+    "url": "/chores/static/js/2.d158152c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/chores/static/js/2.d158152c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8924030eabcd55a5df05",
+    "url": "/chores/static/js/main.ddd033f8.chunk.js"
+  },
+  {
+    "revision": "91735aede66856a1d136",
+    "url": "/chores/static/js/runtime-main.ae0c03c7.js"
+  }
+]);
